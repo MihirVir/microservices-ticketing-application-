4241ec36f078a5d5be2ab4caf0bff4568f531560
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2024-04-22 21:40:09 +0100

    Making error handling code more reusable

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,16 @@
-export class DatabaseConnectionError extends Error {
-  reason = "Error connecting to the database"
+import { CustomError } from "./CustomError";
 
-  constructor(){
-    super();
+export class DatabaseConnectionError extends CustomError {
+  reason = "Error connecting to the database";
+  status = 500;
+
+  constructor() {
+    super("Error connecting to the database");
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
